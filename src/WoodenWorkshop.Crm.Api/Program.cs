8e8fb0298ced80b713ab27bca118d96f6683ef17
Slava Germonenko <slava.gemonenko@gmail.com>
2021-12-20 15:45:59 +0300

    A small fix for dev env

--- src/WoodenWorkshop.Crm.Api/Program.cs
+++ src/WoodenWorkshop.Crm.Api/Program.cs
@@ -46,8 +46,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseHttpsRedirection();
+}
 
-app.UseHttpsRedirection();
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseAuthorization();
 app.MapControllers();
